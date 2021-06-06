b8901b283db54a5112a89753e09ef15a666bfbc8
marek-cel <46421808+marek-cel@users.noreply.github.com>
2021-06-06 11:47:05 +0200

    binary package creation script

---
+++ make_zip.bat
@@ -0,0 +1,24 @@
+copy src\bscaero.ico bscaero.ico
+
+call %QTDIR%/bin/qtenv2.bat
+
+cd /D "%~dp0"
+
+echo %cd%
+
+rmdir /S /Q bscaero
+
+mkdir bscaero
+mkdir bscaero\bin
+mkdir bscaero\data
+copy /y bin\*.exe bscaero\bin
+xcopy data bscaero\data /E
+
+rem cd qt\bin
+rem call "%QTDIR%\bin\windeployqt.exe" --release mscsim.exe
+rem cd ..\..
+call "%QTDIR%\bin\windeployqt.exe" --release bscaero\bin\bscaero.exe
+
+"C:\Program Files\7-Zip\7z.exe" a bscaero_0.1_win64.zip -r bscaero\
+
+pause
